a308e6a5e4cfe69a9d899402aaa3ebf1f2de4362
Edwin <46379855+Ebi2626@users.noreply.github.com>
2020-01-01 20:11:47 +0100

    Add array with skills
    Creating array of objects with my skills

---
+++ src/skillsList.js
@@ -0,0 +1,81 @@
+const SkillsList = [
+  {
+    name: "HTML 5",
+    level: "Medium",
+    description:
+      "I have quite big experience in writing code in HTML. I can build website structures without using documentation or any other source. I understand all basics tags and I know semantic tags from HTML 5. If I would have to write some code in HTML 5 I'd like to use emmet to write more code in less time."
+  },
+  {
+    name: "CSS 3",
+    level: "Medium",
+    description:
+      "In my projects I usually use BEM methodolgy to ordered my css classes. I understand idea of mobile first approach and I know problems of writing styles for responsive layout. In more advanced projects I use SASS (SCSS) with webpack to write more code in shorter time."
+  },
+  {
+    name: "JavaScript",
+    level: "Medium",
+    description:
+      "I know both ES5 and ES6 version of Ecma Script. I usually try to programming in functional paradigm and make my code cleaner through writing it in some order: event listeners at the end of the code, declaring varibles at the beginning of each function. A little comments before each function. I can use JS without docummentation in the basic task. More advenced task I usually bootstrap with Stack Overflow or programmers groups on facebook."
+  },
+  {
+    name: "React",
+    level: "Beginner",
+    description:
+      "I understand concept of this library. I can build functional and class components without docummentation. I also understand Redux, Styled Components, React Transition Grup and Theme Provider. That mixture of tools let me writing well looks applications."
+  },
+  {
+    name: "PHP",
+    level: "Beginner",
+    description:
+      "I understand grammar of that language. I know the most popular way to manage common backend problems. I can make connections to MySql database, adding and removing records. I can also use RegEx to connect function behaviour with the URI content which is helpfull to work with WordPress. I like combining PHP functions with JS code, which let me for much more than same JS."
+  },
+  {
+    name: "Webpack",
+    level: "Enrty",
+    description:
+      "With docummentation I can configure this tool to automatize basics actions necessary to set up more complicated project."
+  },
+  {
+    name: "Git",
+    level: "Enrty",
+    description:
+      "I understand concept of Git and the way to manage project which its authors propose. I can making commits with Git bash and copiyng remote repository to my hard drive from Github."
+  },
+  {
+    name: "Github",
+    level: "Medium",
+    description:
+      "From almost beginning of my programming path I use Github as my own cloud to store my code for free. For some time I also use gihub pages to chcecking my projects in real life and sharing my code with friends."
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  },
+  {
+    name: "",
+    level: "",
+    description: ""
+  }
+];
+export default SkillsList;
