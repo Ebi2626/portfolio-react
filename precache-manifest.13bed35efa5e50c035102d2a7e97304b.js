5eca492e2524b8106bfe0c16db120fed5c60e490
edwin <edwin.harmata@gmail.com>
2020-01-21 22:32:27 +0100

    Updates

---
+++ precache-manifest.13bed35efa5e50c035102d2a7e97304b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b50e93838d7db102ac5790bd631b3a7",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "cc0ec60007a29f68178f",
+    "url": "/portfolio-react/static/css/main.a2487fa3.chunk.css"
+  },
+  {
+    "revision": "3768ed3922f0d3312278",
+    "url": "/portfolio-react/static/js/2.0bb643fb.chunk.js"
+  },
+  {
+    "revision": "cc0ec60007a29f68178f",
+    "url": "/portfolio-react/static/js/main.f06641ce.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  }
+]);
