0b16547904ee5b39b74b442d53064fa0dfa237e3
Edwin Harmata <edwin.harmata@gmail.com>
2020-01-25 19:41:36 +0100

    Updates

---
+++ precache-manifest.ad4bd9e5673266fc75dfd36cf9569a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab41d34c1e1ea1c7afe8bae90ad5d58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42e5aa4cf826ba12b006",
+    "url": "./static/css/main.cab7b3a6.chunk.css"
+  },
+  {
+    "revision": "c2530f880f567b6e4160",
+    "url": "./static/js/2.b4eea77c.chunk.js"
+  },
+  {
+    "revision": "42e5aa4cf826ba12b006",
+    "url": "./static/js/main.d6f72b8c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
